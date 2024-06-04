57993fd6ff1547daf35f039604d7234704b89db0
Yana Temirgalieva <kolesiko.yana@gmail.com>
2024-06-04 12:53:02 +0300

    makes settings for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import HistoryRouter from './components/history-route/history-route';
+import browserHistory from './browser-history';
 import App from './App.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HistoryRouter history={browserHistory} basename={import.meta.env.BASE_URL}>
+      <App />
+    </HistoryRouter>
   </React.StrictMode>,
 )
